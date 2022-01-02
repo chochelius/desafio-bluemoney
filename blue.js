9c9601cbb64ba5409caa51d1f5a0c451871156ab
chochelius <chochelius@gmail.com>
2022-01-02 16:22:16 -0300

    2nd commit

--- blue.js
+++ blue.js
@@ -11,40 +11,37 @@ let cantidad = Number(params[3]);
 let datRec = {};
 
 https
- .get(url, (resp) => {
- let data = "";
- resp.on("data", (chunk) => {
- data += chunk;
- });
- resp.on("end", () => {
- datRec = JSON.parse(data);
- const valor = datRec[indicador].valor;
- console.log(
+  .get(url, (resp) => {
+    let data = "";
+    resp.on("data", (chunk) => {
+      data += chunk;
+    });
+    resp.on("end", () => {
+      datRec = JSON.parse(data);
+      const valor = datRec[indicador].valor;
+      console.log(
+        `
+    A la fecha: ${new Date()}
+    Fue realizada cotización con los siguientes datos:
+    Cantidad de pesos a convertir: ${cantidad} pesos
+    Convertido a "${indicador}" da un total de:
+    ${Math.trunc(cantidad / valor)}
     `
+      );
+      fs.writeFile(
+        nombre + "." + extension,
+        `
     A la fecha: ${new Date()}
     Fue realizada cotización con los siguientes datos:
     Cantidad de pesos a convertir: ${cantidad} pesos
     Convertido a "${indicador}" da un total de:
     ${Math.trunc(cantidad / valor)}
     `,
- );
- fs.writeFile(
-    nombre + "." + extension,
-    `
-    A la fecha: ${new Date()}
-    Fue realizada cotización con los siguientes datos:
-    Cantidad de pesos a convertir: ${cantidad} pesos
-    Convertido a "${indicador}" da un total de:
-    ${Math.trunc(cantidad / valor)}
-    `,
-    "utf8",
-    () => {}
-  );
- });
- })
- .on("error", (err) => console.error(err.message));
+        "utf8",
+        () => {}
+      );
+    });
+  })
+  .on("error", (err) => console.error(err.message));
 
-
-
-
- 
+  // fin del código
